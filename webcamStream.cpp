952fbabbbba22401ccfb565bff21d45114b9cf31
ha1984 <henningahrens@hotmail.de>
2017-04-02 19:57:09 +0200

    Add files via upload

---
+++ webcamStream.cpp
@@ -0,0 +1,18 @@
+﻿#include <opencv2\highgui\highgui.hpp>
+
+int main()
+{
+	cvNamedWindow("Video", CV_WINDOW_AUTOSIZE);
+	CvCapture* capture = cvCreateCameraCapture(0);
+	IplImage* frame;
+	while (1) {
+		frame = cvQueryFrame(capture);
+		if (!frame) break;
+		cvShowImage("Video", frame);
+		char c = cvWaitKey(33);
+		if (c == 27) break;
+	}
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("Video");
+	return 0;
+}
